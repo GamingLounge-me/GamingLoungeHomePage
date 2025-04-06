3dbb04a8082961038ea37adc078065994e7f7562
Jon1Games <118659471+Jon1Games@users.noreply.github.com>
2025-04-06 12:55:52 +0200

    fix spacing

--- src/pages/minecraft/hypixel/arcade/zombies.tsx
+++ src/pages/minecraft/hypixel/arcade/zombies.tsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom'
 const Zombies = () => {
     return (
         <>
-            <div className="full-block right minecraft-hypixel-zombies-dead_end">
+            <div className="full-block right empty minecraft-hypixel-zombies-dead_end">
                 <h1><a id="dead_end" href="#dead_end">Dead End</a></h1>
             </div>
 
-            <div className="full-block right minecraft-hypixel-zombies-bad_blood">
+            <div className="full-block right empty minecraft-hypixel-zombies-bad_blood">
                 <h1><a id="bad_blood" href="#bad_blood">Bad Blood</a></h1>
             </div>
 
-            <div className="full-block right minecraft-hypixel-zombies-alien_arcadium">
+            <div className="full-block right empty minecraft-hypixel-zombies-alien_arcadium">
                 <h1><a id="alien_arcadium" href="#alien_arcadium">Alien Arcadium</a></h1>
                 <button><Link to="/minecraft/hypixel/arcade/zombies/alien_arcadium/">more information</Link></button>
             </div >
 
 
-            <div className="full-block right minecraft-hypixel-zombies-prison">
+            <div className="full-block right empty minecraft-hypixel-zombies-prison">
                 <h1><a id="prison" href="#prison">Prison</a></h1>
             </div>
         </>

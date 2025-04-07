c2aa8bd0031a7ee3e9a38ed540a232a1b0cb728d
lordgkram <52086058+lordgkram@users.noreply.github.com>
2025-04-07 17:07:07 +0200

    updated landing page style

---
+++ src/components/Block.tsx
@@ -0,0 +1,64 @@
+import { CSSProperties, FunctionComponent, PropsWithChildren, ReactNode } from 'react';
+import { Link } from 'react-router-dom'
+
+type TextColor = "light" | "dark" | "theme";
+type Position = "right" | "left" | "center";
+
+type BlockProps = {
+    section?: string,
+    title: ReactNode,
+    position?: Position,
+    image?: string,
+    textColor?: TextColor,
+};
+
+const getTextColorStyle: (color?: TextColor) => string = (color) => {
+    switch(color) {
+        case 'dark': return "text-neutral ";
+        case 'light': return "text-neutral-content ";
+        case 'theme':
+        default:
+            return "";
+    }
+};
+
+const getPositionStyle: (position?: Position) => string = (position) => {
+    switch(position) {
+        case 'right': return "justify-self-end ";
+        case 'left': return "justify-self-start ";
+        case 'center':
+        default:
+            return "justify-self-center ";
+    }
+};
+
+export const BlockHolder: FunctionComponent<PropsWithChildren> = ({children}) => {
+    return (
+        <div className="py-20 grid grid-cols-1 gap-y-10 md:px-10">
+            {children}
+        </div>
+    );
+};
+
+export const Block: FunctionComponent<PropsWithChildren<BlockProps>> = ({section, title, position, image, textColor, children}) => {
+    return (
+        <div
+                id={section}
+                className={
+                    (image ? "bg-(image:--img-bg-card) bg-local bg-center bg-no-repeat bg-cover " : "bg-base-100 ") +
+                    getPositionStyle(position) +
+                    "card shadow-sm w-full md:w-172 xl:w-250"}
+                style={image ? {
+                    '--img-bg-card': `url("${image}")`,
+                } as CSSProperties : {}} >
+            <div className={(image ? "backdrop-brightness-70 dark:backdrop-brightness-30 " : "") +
+                    getTextColorStyle(textColor) +
+                    "card-body rounded-[inherit]"}>
+                <h2 className={(section ? "link link-hover " : "") + "text-3xl"}>
+                    {section ? <Link to={"#" + section}>{title}</Link> : title}
+                </h2>
+                {children}
+            </div>
+        </div>
+    );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,62 +1,96 @@
+import { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom'
+import ImgMc from './../assets/images/MC.png?url';
+import ImgCoding from './../assets/images/coding.jpg?url';
+import { Block, BlockHolder } from '../components/Block';
 
-const Home = () => {
+const Home: FunctionComponent = () => {
     return (
-        <>
-            <div className="aboutus" style={{ marginBottom: 350 }} >
-                <h1><a id="aboutus" href="#aboutus">About Us</a></h1>
-            </div >
+        <BlockHolder>
+            <Block
+                    section="aboutus"
+                    title="About Us" >
+                GamingLounge was born from a simple idea: to create a space where passionate gamers can come together to explore their favorite games and discover new ones. We're host a collection of servers. we're a community that values connection, collaboration, and the joy of shared experiences.
 
-            <div className="full-block right minecraft">
-                <h1><a id="endless" href="#endless" style={{ textDecoration: "none" }}>Minecraft: Endless</a></h1><br />
-                <h3>IP: GamingLounge.me</h3><br />
-                <h3>Version: 1.21.4</h3><br />
-                <h4>Join Endless</h4>
-                <p>Right click NPC "Endless" in the lobby or issue /server Endless2 to join the server.</p><br />
-                <h4>Usefull Commands</h4>
-                <ul>
-                    <li>/clan</li>
-                    <li>/pad</li>
-                    <li>/claim</li>
-                    <li>/teamclaim</li>
-                    <li>/tpa</li>
-                    <li>/rtp</li>
-                </ul><br />
-                <h4>Usefull items (Found in Recepie book)</h4>
-                <ul>
-                    <li><a href="https://github.com/GamingLounge-me/PortableInventories" target="_blank">PortableInventories</a>
-                        <ul>
-                            <li>Portable- Workbench / Enderchest / Grindstone / Stonecutter / Smithingtable</li>
-                            <li>The item is not renamed, just the vanilla item but enchanted</li>
-                        </ul>
-                    </li>
-                    <li><a href="https://github.com/GamingLounge-me/telepads" target="_blank">Telepad</a></li>
-                </ul>
-                <button><Link to="/minecraft/endless/">more information</Link></button>
-                <button><a href="https://map.gaminglounge.me/minecraft/endless/" target="_blank">Map</a></button>
-            </div >
+                What sets GamingLounge apart is our commitment to fostering a positive and supportive environment. We believe in playing together, helping each other, and building something great as a team. This extends beyond the games we play and into the realm of coding. We recognize the power of technology and encourage our members to explore their coding interests, share their projects, and learn from one another.
 
-            <div className="full-block left coding">
-                <h1><a id="coding" href="#coding" style={{ textDecoration: "none" }}>Coding</a></h1><br />
-                <h3>Java</h3><br />
-                <h5>API</h5>
-                <ul>
-                    <li><a href="https://github.com/GamingLounge-me/ConfigAPI" target="_blank">ConfigAPI</a></li>
-                    <li><a href="https://github.com/GamingLounge-me/GuiAPI" target="_blank">GuiAPI</a></li>
-                    <li><a href="https://github.com/GamingLounge-me/PlayerInputAPI" target="_blank">PlayerInputAPI</a></li>
-                    <li><a href="https://github.com/GamingLounge-me/ItemBuilder" target="_blank">ItemBuilder</a></li>
-                </ul><br />
-                <h5>Plugins</h5>
-                <ul>
+                Whether you're a seasoned gamer, a curious newcomer, or someone with a passion for coding, GamingLounge offers a place to connect, learn, and have fun. Join us as we continue to build our community and explore the exciting worlds of gaming and technology!
+            </Block>
 
-                </ul><br />
-                <h3>HTML/CSS/JavaScript</h3>
-                <ul>
-                    <li><a href="https://github.com/GamingLounge-me/GamingLoungeHomePage" target="_blank">This Website</a></li>
-                </ul>
-            </ div>
-        </>
+            <Block
+                    section="endless"
+                    title="Minecraft: Endless"
+                    position="right"
+                    image={ImgMc}
+                    textColor="light" >
+                <h3 className="text-xl">IP: <span className="underline">GamingLounge.me</span></h3>
+                <h3 className="text-xl">Version: 1.21.4</h3>
+                <div>
+                    <h4 className="text-lg">Join Endless</h4>
+                    <p>Right click NPC "Endless" in the lobby or issue /server Endless2 to join the server.</p>
+                </div>
+                <div>
+                    <h4 className="text-lg">Usefull Commands</h4>
+                    <ul className="list-disc pl-4">
+                        <li>/clan</li>
+                        <li>/pad</li>
+                        <li>/claim</li>
+                        <li>/teamclaim</li>
+                        <li>/tpa</li>
+                        <li>/rtp</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 className="text-lg">Usefull items (Found in Recepie book)</h4>
+                    <ul className="list-disc pl-4">
+                        <li><a href="https://github.com/GamingLounge-me/PortableInventories" target="_blank" className="link">PortableInventories</a>
+                            <ul className="list-[circle] pl-4">
+                                <li>Portable- Workbench / Enderchest / Grindstone / Stonecutter / Smithingtable</li>
+                                <li>The item is not renamed, just the vanilla item but enchanted</li>
+                            </ul>
+                        </li>
+                        <li><a href="https://github.com/GamingLounge-me/telepads" target="_blank" className="link">Telepad</a></li>
+                    </ul>
+                </div>
+                <div className="card-actions justify-end">
+                    <Link to="/minecraft/endless/" className="btn">More information</Link>
+                    <a href="https://map.gaminglounge.me/minecraft/endless/" target="_blank" className="btn">Map</a>
+                </div>
+            </Block>
+
+            <Block
+                    title="Coding"
+                    section="coding"
+                    image={ImgCoding}
+                    position="left"
+                    textColor="light" >
+                {/* TODO: abstaction */}
+                <h3 className="text-xl">Java</h3>
+                <div>
+                    <h4 className="text-lg">API</h4>
+                    <ul className="list-disc pl-4">
+                        <li><a href="https://github.com/GamingLounge-me/ConfigAPI" target="_blank" className="link">ConfigAPI</a></li>
+                        <li><a href="https://github.com/GamingLounge-me/GuiAPI" target="_blank" className="link">GuiAPI</a></li>
+                        <li><a href="https://github.com/GamingLounge-me/PlayerInputAPI" target="_blank" className="link">PlayerInputAPI</a></li>
+                        <li><a href="https://github.com/GamingLounge-me/ItemBuilder" target="_blank" className="link">ItemBuilder</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 className="text-lg">Plugins</h4>
+                    <ul className="list-disc pl-4">
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="text-xl">HTML/CSS/JavaScript</h3>
+                    <ul className="list-disc pl-4">
+                        <li><a href="https://github.com/GamingLounge-me/GamingLoungeHomePage" target="_blank" className="link">This Website</a></li>
+                    </ul>
+                </div>
+
+            </Block>
+        </BlockHolder>
     )
 };
 
-export default Home
+export default Home;
+

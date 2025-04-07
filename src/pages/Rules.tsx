f9f997cf6eb140efb9d47c2b97d3ec5e6357600d
lordgkram <52086058+lordgkram@users.noreply.github.com>
2025-04-07 10:51:20 +0200

    updated Rules Page to new Style

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -1,41 +1,129 @@
-var rules = {
+import { FunctionComponent } from "react";
+
+type Rule = {
+    id: string,
+    text: string,
+    childs?: RuleSet,
+};
+
+type RuleSet = Rule[];
+
+type Rules = {
+    [category: string]: {
+        text: string,
+        rules: RuleSet,
+    },
+};
+
+const rules: Rules = {
     general: {
-        "1": "1a",
-        "2": {
-            "1": "1b",
-            "2": "1c"
-        }
+        text: "General",
+        rules: [
+            { id: "r1", text: "First Rule" },
+            {
+                id: "r2",
+                text: "Second Rule",
+                childs: [
+                    {
+                        id: "r1",
+                        text: "exeption",
+                        childs: [
+                            { id: "r1", text: "with exeptions" },
+                        ],
+                    },
+                ],
+            },
+        ],
     },
     discord: {
-        "1": "1a",
-        "2": "1b"
-    }
+        text: "Discord",
+        rules: [
+            { id: "r1", text: "Lorem Ipsum" },
+        ],
+    },
 };
 
-const Rules = () => {
+type HiddenLinkProps = {
+    href: string,
+};
+
+const HiddenLink: FunctionComponent<HiddenLinkProps> = ({href}) => {
     return (
-        <main>
-            <div className="block">
-                <h1>Rules</h1>
-                {Object.entries(rules).map(([category, rules]) => (
-                    <>
-                        <h3><a id={category} href={"#" + category}>{category.charAt(0).toUpperCase() + category.slice(1)}</a></h3>
-                        <ul>
-                            {Object.entries(rules).map(([ruleNumber, ruleText]) => (
-                                <li key={ruleNumber}>
-                                    {ruleNumber}. {typeof ruleText === 'string' ? ruleText : Object.entries(ruleText).map(([subRuleText]) => (
-                                        <ul style={{ listStyle: "circle" }}>
-                                            <li>{subRuleText}</li>
-                                        </ul>
-                                    ))}
-                                </li>
-                            ))}
-                        </ul>
-                    </>
-                ))}
-            </div>
-        </main >
-    )
+        <a href={href} className="text-[rgb(0,0,0,0)] link link-hover link-primary">#</a>
+    );
 };
 
-export default Rules
+type RuleRenderData = {
+    level: number,
+    path: string,
+};
+
+type RuleRenderProps = {
+    rule: Rule,
+    data: RuleRenderData,
+};
+
+type RuleSetRenderProps = {
+    rules: RuleSet,
+    data: RuleRenderData,
+};
+
+const RuleRenderer: FunctionComponent<RuleRenderProps> = ({rule, data}) => {
+    return (
+        <li id={data.path}>
+            {rule.text}
+            <HiddenLink href={"#" + data.path} />
+            {rule.childs && <RuleSetRenderer rules={rule.childs} data={data} />}
+        </li>
+    );
+};
+
+const listStyle: (data: RuleRenderData) => string = (data) => {
+    switch(data.level) {
+        case 0: return "list-decimal ";
+        case 1: return "list-[lower-alpha] ";
+        case 2: return "list-[lower-roman] ";
+        default: return "list-decimal ";
+    };
+};
+
+const nextLevel: (data: RuleRenderData, childId: string) => RuleRenderData = (data, childId) => {
+    return {
+        level: data.level + 1,
+        path: data.path + "-" + childId,
+    };
+};
+
+const RuleSetRenderer: FunctionComponent<RuleSetRenderProps> = ({rules, data}) => {
+    return (
+        <ol className={(listStyle(data)) + "pl-8"}>
+            {rules.map((rule, idx) => (<RuleRenderer rule={rule} data={nextLevel(data, rule.id)} key={idx} />))}
+        </ol>
+    );
+};
+
+
+const RulesPage: FunctionComponent = () => {
+    return (
+        <div className="py-20 justify-center flex">
+            <div className="p-5 card bg-base-100 shadow-sm md:w-128 w-full">
+                <h1 className="text-5xl">Rules</h1>
+                {Object.entries(rules).map(([category, rules], idx) => (
+                    <div id={category} key={idx}>
+                        <div className="text-3xl">
+                            {rules.text}
+                            <HiddenLink href={"#" + category} />
+                        </div>
+                        <RuleSetRenderer rules={rules.rules} data={{
+                            level: 0,
+                            path: category,
+                        }} />
+                    </div>
+                ))}
+                </div>
+        </div>
+    );
+};
+
+export default RulesPage;
+

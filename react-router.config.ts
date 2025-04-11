59334c0ea6b7110d26bfee536d60a3da57b5d8c7
lordgkram <52086058+lordgkram@users.noreply.github.com>
2025-04-11 23:46:07 +0200

    added prebuild configuration

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,14 @@ import type { Config } from "@react-router/dev/config";
 export default {
     appDirectory: "src",
     ssr: false,
+    prerender: [
+        "/",
+        "/rules",
+        "/minecraft/endless",
+        "/minecraft/hypixel/arcade/zombies",
+        "/minecraft/hypixel/arcade/zombies/alien_arcadium",
+        "/os/linux/commands",
+        "/hypervisor/proxmox",
+    ],
 } as Config;
 
